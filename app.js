79dfd3482ea61e570d0c87444b4c188985d9bb58
mmifs <85654339+mmifs@users.noreply.github.com>
2021-08-11 19:37:34 -0400

    creates basic portfolio with user input x,y

--- app.js
+++ app.js
@@ -1,19 +1,36 @@
-const profileDataArgs = process.argv.slice(2);
+const fs = require("fs");
+
+const profileDataArgs = process.argv.slice(2, process.argv.length); // captures the input via node terminal
+
+const [userName, github] = profileDataArgs; // assigns name, github to profileDataArgs[1, 2]
+
+const generatePage = (userName, github) => {
+  return `
+  <!DOCTYPE html> 
+  <html lang="en"> 
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Portfolio Demo</title>
+  </head>
+
+  <body>
+    <h1>${userName}</h1>
+    <h2><a href="https://github.com/${github}">Github</a></h2>
+  </body>
+  </html>
+  `;
+};
 
 const printProfileData = profileDataArr => {
-  // This...
-  for (let i = 0; i < profileDataArr.length; i += 1) {
-    console.log(profileDataArr[i]);
-  }
 
-  console.log('================');
-
-  // Is the same as this...
   profileDataArr.forEach(profileItem => console.log(profileItem));
 
-  console.log('================');
-
-  console.log(process.argv);
 };
 
 printProfileData(profileDataArgs);
+fs.writeFile('index.html', generatePage(userName, github), err => {
+  if (err) throw err;
+  console.log('Portfolio complete, open html to view')
+});
